e3c9c1d25524daca64f3b425d1d4fe21a25b4a4d
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-05 16:58:17 -0600

    Add version information methods

--- spec/espresso_spec.cr
+++ spec/espresso_spec.cr
@@ -1,4 +1,33 @@
 require "./spec_helper"
 
 Spectator.describe Espresso do
+  let(major) { 3 }
+  let(minor) { 3 }
+  let(patch) { 0 }
+
+  describe "#version" do
+    subject { Espresso.version }
+
+    it { is_expected.to have_attributes(major: major, minor: minor, patch: patch) }
+  end
+
+  describe "#compiled_version" do
+    subject { Espresso.compiled_version }
+
+    it { is_expected.to have_attributes(major: major, minor: minor, patch: patch) }
+  end
+
+  describe "#runtime_version" do
+    subject { Espresso.runtime_version }
+
+    it { is_expected.to have_attributes(major: major, minor: minor, patch: patch) }
+  end
+
+  describe "#version_string" do
+    subject { Espresso.version_string }
+
+    it "contains the version" do
+      is_expected.to contain(Espresso.version.to_s)
+    end
+  end
 end
--- src/espresso.cr
+++ src/espresso.cr
@@ -1,4 +1,42 @@
+require "glfw"
+require "semantic_version"
+
 # Lightweight wrapper around GLFW for Crystal.
 module Espresso
+  extend self
+
+  # Current version of the shard.
   VERSION = "0.1.0"
+
+  # Version of GLFW that Espresso was compiled against.
+  # This should match `#runtime_version` to have consistent/expected behavior.
+  # A `SemanticVersion` is returned.
+  def compiled_version
+    SemanticVersion.new(
+      LibGLFW::VERSION_MAJOR,
+      LibGLFW::VERSION_MINOR,
+      LibGLFW::VERSION_REVISION
+    )
+  end
+
+  # Version of GLFW that is loaded and in-use by Espresso.
+  # This should match `#compiled_version` to have consistent/expected behavior.
+  # A `SemanticVersion` is returned.
+  def runtime_version
+    LibGLFW.get_version(out major, out minor, out revision)
+    SemanticVersion.new(major, minor, revision)
+  end
+
+  # Version of GLFW that is loaded and in-use by Espresso.
+  # A `SemanticVersion` is returned.
+  def version
+    runtime_version
+  end
+
+  # Compiled version string produced by GLFW.
+  # Includes the version string and
+  # additional compilation and environment information.
+  def version_string
+    String.new(LibGLFW.get_version_string)
+  end
 end
