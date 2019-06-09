b9d5004a738b82b11fbb9a9cffc6ab5b620f1702
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-08 23:03:38 -0600

    Add position struct

---
+++ src/espresso/position.cr
@@ -0,0 +1,25 @@
+module Espresso
+  # Two-dimensional position of an object.
+  # Contains an x and y-coordinate
+  # represented as 32-bit integers.
+  struct Position
+    # Distance along the x-axis.
+    getter x : Int32
+
+    # Distance along the y-axis.
+    getter y : Int32
+
+    # Creates the position with initial values.
+    def initialize(@x, @y)
+    end
+
+    # Creates a string representation of the position.
+    def to_s(io)
+      io << '('
+      io << @x
+      io << ", "
+      io << @y
+      io << ')'
+    end
+  end
+end
