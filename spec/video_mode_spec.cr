f2253bb789a6ab9ddc22b62b2cb5a886cb481fd7
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-09 10:54:51 -0600

    Add video mode struct

---
+++ spec/video_mode_spec.cr
@@ -0,0 +1,88 @@
+require "./spec_helper"
+
+Spectator.describe Espresso::VideoMode do
+  # Some very strange monitor configuration.
+  let(width) { 1920 }
+  let(height) { 1080 }
+  let(red) { 3 }
+  let(green) { 4 }
+  let(blue) { 5 }
+  let(rate) { 120 }
+  subject(video_mode) { described_class.new(width, height, red, green, blue, rate) }
+
+  it "stores the width" do
+    expect(video_mode.width).to eq(width)
+  end
+
+  it "stores the height" do
+    expect(video_mode.height).to eq(height)
+  end
+
+  it "stores the red bit depth" do
+    expect(video_mode.red).to eq(red)
+  end
+
+  it "stores the green bit depth" do
+    expect(video_mode.green).to eq(green)
+  end
+
+  it "stores the blue bit depth" do
+    expect(video_mode.blue).to eq(blue)
+  end
+
+  it "stores the refresh rate" do
+    expect(video_mode.refresh_rate).to eq(rate)
+  end
+
+  describe "#size" do
+    subject { video_mode.size }
+
+    it "contains the correct width and height" do
+      is_expected.to have_attributes(width: width, height: height)
+    end
+  end
+
+  describe "#depth" do
+    subject { video_mode.depth }
+
+    it "is the sum of all color channels" do
+      is_expected.to eq(red + green + blue)
+    end
+  end
+
+  describe "#to_s" do
+    subject { video_mode.to_s }
+
+    it "is formatted correctly" do
+      is_expected.to match(/^\d+x\d+x\d+@\d+Hz \(R\dG\dB\d\)$/)
+    end
+
+    it "contains the width" do
+      is_expected.to start_with("#{width}x")
+    end
+
+    it "contains the height" do
+      is_expected.to contain("x#{height}x")
+    end
+
+    it "contains the depth" do
+      is_expected.to contain("x#{red + green + blue}@")
+    end
+
+    it "contains the refresh rate" do
+      is_expected.to contain("@#{rate}Hz")
+    end
+
+    it "contains the red bit depth" do
+      is_expected.to contain("R#{red}")
+    end
+
+    it "contains the green bit depth" do
+      is_expected.to contain("G#{green}")
+    end
+
+    it "contains the blue bit depth" do
+      is_expected.to contain("B#{blue}")
+    end
+  end
+end
---
+++ src/espresso/video_mode.cr
@@ -0,0 +1,70 @@
+require "glfw"
+require "./size"
+
+module Espresso
+  # Information about the size, color depth, and refresh rate of a monitor.
+  struct VideoMode
+    # The width, in screen coordinates, of the video mode.
+    getter width : Int32
+
+    # The height, in screen coordinates, of the video mode.
+    getter height : Int32
+
+    # The bit depth of the red channel of the video mode.
+    getter red : Int32
+
+    # The bit depth of the green channel of the video mode.
+    getter green : Int32
+
+    # The bit depth of the blue channel of the video mode.
+    getter blue : Int32
+
+    # The refresh rate, in Hz, of the video mode.
+    getter refresh_rate : Int32
+
+    # Creates a new video mode.
+    # This initializer is typically used for constructing a desired video mode.
+    # Then the desired video mode can be compared against supported modes with `#==`.
+    def initialize(@width, @height, @red, @green, @blue, @refresh_rate)
+    end
+
+    # Creates a video mode from an existing one.
+    # Copies the values from a GLFW struct.
+    protected def initialize(video_mode : LibGLFW::VideoMode)
+      @width = video_mode.width
+      @height = video_mode.height
+      @red = video_mode.red_bits
+      @green = video_mode.green_bits
+      @blue = video_mode.blue_bits
+      @refresh_rate = video_mode.refresh_rate
+    end
+
+    # Width and height of the video mode.
+    def size
+      Size.new(@width, @height)
+    end
+
+    # Total color depth of all channels.
+    def depth
+      @red + @blue + @green
+    end
+
+    # Creates a string representation of the video mode.
+    def to_s(io)
+      io << @width
+      io << 'x'
+      io << @height
+      io << 'x'
+      io << depth
+      io << '@'
+      io << @refresh_rate
+      io << "Hz (R"
+      io << @red
+      io << 'G'
+      io << @green
+      io << 'B'
+      io << @blue
+      io << ')'
+    end
+  end
+end
