1428a30681e3d083b000df7d941fde825301bd2b
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-24 19:49:35 -0600

    Move events down (under the initializers)

--- src/espresso/window.cr
+++ src/espresso/window.cr
@@ -184,116 +184,6 @@ module Espresso
       end
     end
 
-    # Registers a listener to respond when the window is moved.
-    # The block of code passed to this method will be invoked when the event occurs.
-    # A `WindowMoveEvent` instance will be passed to the block as an argument,
-    # which contains all relevant information about the event.
-    # To remove the listener, call `#remove_move_listener` with the proc returned by this method.
-    #
-    # **Wayland:** This callback will never be called,
-    # as there is no way for an application to know its global position.
-    event move, WindowMoveEvent, set_window_pos_callback
-
-    # Registers a listener to respond when the window's size changes.
-    # The block of code passed to this method will be invoked when the event occurs.
-    # A `WindowResizeEvent` instance will be passed to the block as an argument,
-    # which contains all relevant information about the event.
-    # To remove the listener, call `#remove_resize_listener` with the proc returned by this method.
-    event resize, WindowResizeEvent, set_window_size_callback
-
-    # Registers a listener to respond when the user requests the window to be closed.
-    # The block of code passed to this method will be invoked when the event occurs.
-    # A `WindowClosingEvent` instance will be passed to the block as an argument,
-    # which contains all relevant information about the event.
-    # To remove the listener, call `#remove_closing_listener` with the proc returned by this method.
-    #
-    # The `#closing?` flag is set before this callback is called,
-    # but you can modify it at any time with `#closing=`.
-    #
-    # **macOS:** Selecting Quit from the application menu will trigger the close callback for all windows.
-    event closing, WindowClosingEvent, set_window_close_callback
-
-    # Registers a listener to respond when the window's contents need to be redrawn,
-    # for example if the window has been exposed after having been covered by another window.
-    #
-    # On compositing window systems such as Aero, Compiz, Aqua or Wayland,
-    # where the window contents are saved off-screen,
-    # this callback may be called only very infrequently or never at all.
-    #
-    # The block of code passed to this method will be invoked when the event occurs.
-    # A `WindowRefreshEvent` instance will be passed to the block as an argument,
-    # which contains all relevant information about the event.
-    # To remove the listener, call `#remove_refresh_listener` with the proc returned by this method.
-    event refresh, WindowRefreshEvent, set_window_refresh_callback
-
-    # Registers a listener to respond when the window gains or loses focus.
-    #
-    # After the focus callback is called for a window that lost input focus,
-    # synthetic key and mouse button release events will be generated for all such that had been pressed.
-    # For more information, see `glfwSetKeyCallback` and `glfwSetMouseButtonCallback`.
-    #
-    # The block of code passed to this method will be invoked when the request occurs.
-    # A `WindowFocusEvent` instance will be passed to the block as an argument,
-    # which contains all relevant information about the event.
-    # To remove the listener, call `#remove_focus_listener` with the proc returned by this method.
-    event focus, WindowFocusEvent, set_window_focus_callback
-
-    # Registers a listener to respond when the window is iconified (minimized) or restored from being iconified.
-    # The block of code passed to this method will be invoked when the event occurs.
-    # A `WindowIconifyEvent` instance will be passed to the block as an argument,
-    # which contains all relevant information about the event.
-    # To remove the listener, call `#remove_iconify_listener` with the proc returned by this method.
-    event iconify, WindowIconifyEvent, set_window_iconify_callback
-
-    # Registers a listener to respond when the window is maximized or restored from being maximized.
-    # The block of code passed to this method will be invoked when the event occurs.
-    # A `WindowMaximizeEvent` instance will be passed to the block as an argument,
-    # which contains all relevant information about the event.
-    # To remove the listener, call `#remove_maximize_listener` with the proc returned by this method.
-    event maximize, WindowMaximizeEvent, set_window_maximize_callback
-
-    # Registers a listener to respond when the window's framebuffer is resized.
-    # The block of code passed to this method will be invoked when the event occurs.
-    # A `WindowResizeEvent` instance will be passed to the block as an argument,
-    # which contains all relevant information about the event.
-    # To remove the listener, call `#remove_framebuffer_resize_listener` with the proc returned by this method.
-    event framebuffer_resize, WindowResizeEvent, set_framebuffer_size_callback
-
-    # Registers a listener to respond when the window's content scaling changes.
-    # The block of code passed to this method will be invoked when the event occurs.
-    # A `WindowScaleEvent` instance will be passed to the block as an argument,
-    # which contains all relevant information about the event.
-    # To remove the listener, call `#remove_scale_listener` with the proc returned by this method.
-    event scale, WindowScaleEvent, set_window_content_scale_callback
-
-    # Registers a listener to respond when the window is iconified (minimized) or restored from being iconified.
-    # The block of code passed to this method will be invoked when the event occurs.
-    # A `WindowIconifyEvent` instance will be passed to the block as an argument,
-    # which contains all relevant information about the event.
-    # To remove the listener, call `#remove_iconify_listener` with the proc returned by this method.
-    def on_minimize(&block : WindowIconifyEvent ->)
-      on_iconify(&block)
-    end
-
-    # Removes a previously registered listener that responded to the `#on_minimize` callback.
-    # The *proc* argument should be the return value of the `#on_minimize` method.
-    def remove_minimize_listener(proc : WindowIconifyEvent ->) : Nil
-      remove_iconify_listener(proc)
-    end
-
-    # Removes all previously registered listeners for all window events.
-    private def remove_all_listeners
-      clear_move_listeners
-      clear_resize_listeners
-      clear_closing_listeners
-      clear_refresh_listeners
-      clear_focus_listeners
-      clear_iconify_listeners
-      clear_maximize_listeners
-      clear_framebuffer_resize_listeners
-      clear_scale_listeners
-    end
-
     # Creates a window object by wrapping a GLFW window pointer.
     protected def initialize(@pointer : LibGLFW::Window)
     end
@@ -560,6 +450,116 @@ module Espresso
       ensure
         window.destroy!
       end
+    end
+
+    # Registers a listener to respond when the window is moved.
+    # The block of code passed to this method will be invoked when the event occurs.
+    # A `WindowMoveEvent` instance will be passed to the block as an argument,
+    # which contains all relevant information about the event.
+    # To remove the listener, call `#remove_move_listener` with the proc returned by this method.
+    #
+    # **Wayland:** This callback will never be called,
+    # as there is no way for an application to know its global position.
+    event move, WindowMoveEvent, set_window_pos_callback
+
+    # Registers a listener to respond when the window's size changes.
+    # The block of code passed to this method will be invoked when the event occurs.
+    # A `WindowResizeEvent` instance will be passed to the block as an argument,
+    # which contains all relevant information about the event.
+    # To remove the listener, call `#remove_resize_listener` with the proc returned by this method.
+    event resize, WindowResizeEvent, set_window_size_callback
+
+    # Registers a listener to respond when the user requests the window to be closed.
+    # The block of code passed to this method will be invoked when the event occurs.
+    # A `WindowClosingEvent` instance will be passed to the block as an argument,
+    # which contains all relevant information about the event.
+    # To remove the listener, call `#remove_closing_listener` with the proc returned by this method.
+    #
+    # The `#closing?` flag is set before this callback is called,
+    # but you can modify it at any time with `#closing=`.
+    #
+    # **macOS:** Selecting Quit from the application menu will trigger the close callback for all windows.
+    event closing, WindowClosingEvent, set_window_close_callback
+
+    # Registers a listener to respond when the window's contents need to be redrawn,
+    # for example if the window has been exposed after having been covered by another window.
+    #
+    # On compositing window systems such as Aero, Compiz, Aqua or Wayland,
+    # where the window contents are saved off-screen,
+    # this callback may be called only very infrequently or never at all.
+    #
+    # The block of code passed to this method will be invoked when the event occurs.
+    # A `WindowRefreshEvent` instance will be passed to the block as an argument,
+    # which contains all relevant information about the event.
+    # To remove the listener, call `#remove_refresh_listener` with the proc returned by this method.
+    event refresh, WindowRefreshEvent, set_window_refresh_callback
+
+    # Registers a listener to respond when the window gains or loses focus.
+    #
+    # After the focus callback is called for a window that lost input focus,
+    # synthetic key and mouse button release events will be generated for all such that had been pressed.
+    # For more information, see `glfwSetKeyCallback` and `glfwSetMouseButtonCallback`.
+    #
+    # The block of code passed to this method will be invoked when the request occurs.
+    # A `WindowFocusEvent` instance will be passed to the block as an argument,
+    # which contains all relevant information about the event.
+    # To remove the listener, call `#remove_focus_listener` with the proc returned by this method.
+    event focus, WindowFocusEvent, set_window_focus_callback
+
+    # Registers a listener to respond when the window is iconified (minimized) or restored from being iconified.
+    # The block of code passed to this method will be invoked when the event occurs.
+    # A `WindowIconifyEvent` instance will be passed to the block as an argument,
+    # which contains all relevant information about the event.
+    # To remove the listener, call `#remove_iconify_listener` with the proc returned by this method.
+    event iconify, WindowIconifyEvent, set_window_iconify_callback
+
+    # Registers a listener to respond when the window is maximized or restored from being maximized.
+    # The block of code passed to this method will be invoked when the event occurs.
+    # A `WindowMaximizeEvent` instance will be passed to the block as an argument,
+    # which contains all relevant information about the event.
+    # To remove the listener, call `#remove_maximize_listener` with the proc returned by this method.
+    event maximize, WindowMaximizeEvent, set_window_maximize_callback
+
+    # Registers a listener to respond when the window's framebuffer is resized.
+    # The block of code passed to this method will be invoked when the event occurs.
+    # A `WindowResizeEvent` instance will be passed to the block as an argument,
+    # which contains all relevant information about the event.
+    # To remove the listener, call `#remove_framebuffer_resize_listener` with the proc returned by this method.
+    event framebuffer_resize, WindowResizeEvent, set_framebuffer_size_callback
+
+    # Registers a listener to respond when the window's content scaling changes.
+    # The block of code passed to this method will be invoked when the event occurs.
+    # A `WindowScaleEvent` instance will be passed to the block as an argument,
+    # which contains all relevant information about the event.
+    # To remove the listener, call `#remove_scale_listener` with the proc returned by this method.
+    event scale, WindowScaleEvent, set_window_content_scale_callback
+
+    # Registers a listener to respond when the window is iconified (minimized) or restored from being iconified.
+    # The block of code passed to this method will be invoked when the event occurs.
+    # A `WindowIconifyEvent` instance will be passed to the block as an argument,
+    # which contains all relevant information about the event.
+    # To remove the listener, call `#remove_iconify_listener` with the proc returned by this method.
+    def on_minimize(&block : WindowIconifyEvent ->)
+      on_iconify(&block)
+    end
+
+    # Removes a previously registered listener that responded to the `#on_minimize` callback.
+    # The *proc* argument should be the return value of the `#on_minimize` method.
+    def remove_minimize_listener(proc : WindowIconifyEvent ->) : Nil
+      remove_iconify_listener(proc)
+    end
+
+    # Removes all previously registered listeners for all window events.
+    private def remove_all_listeners
+      clear_move_listeners
+      clear_resize_listeners
+      clear_closing_listeners
+      clear_refresh_listeners
+      clear_focus_listeners
+      clear_iconify_listeners
+      clear_maximize_listeners
+      clear_framebuffer_resize_listeners
+      clear_scale_listeners
     end
 
     # Checks whether the window should be closed.
