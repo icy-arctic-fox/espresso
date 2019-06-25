38e7ec22ea82888edabf4c2635b2de31ad52308f
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-24 22:56:08 -0600

    Add mouse input mode methods

--- src/espresso/mouse.cr
+++ src/espresso/mouse.cr
@@ -200,6 +200,112 @@ module Espresso
       raise NotImplementedError.new("Mouse#cursor=")
     end
 
+    # Retrieves the current cursor mode for the mouse.
+    # This will be one of the values from `CursorMode`.
+    #
+    # By default, the cursor mode is `CursorMode::Normal`,
+    # meaning the regular arrow cursor (or another cursor set with `#cursor=`)
+    # is used and cursor motion is not limited.
+    def mode
+      value = expect_truthy { LibGLFW.get_input_mode(@pointer, LibGLFW::InputMode::Cursor) }
+      CursorMode.value_from(value.to_i)
+    end
+
+    # Checks whether the cursor is hidden and locked.
+    #
+    # See also: `#disable`
+    def disabled?
+      mode.disabled?
+    end
+
+    # Hides and grabs the cursor, providing virtual and unlimited cursor movement.
+    # This is useful for implementing for example 3D camera controls.
+    #
+    # This will hide the cursor and lock it to the window.
+    # GLFW will then take care of all the details of cursor re-centering
+    # and offset calculation and providing the application with a virtual cursor position.
+    # This virtual position is provided normally via `#on_move` and `#position`.
+    #
+    # If you only wish the cursor to become hidden when it is over a window
+    # but still want it to behave normally, use `#hide`.
+    #
+    # Possible errors that could be raised are: `NotInitializedError` and `PlatformError`.
+    #
+    # See also: `#disabled?`
+    def disable
+      checked { LibGLFW.set_input_mode(@pointer, LibGLFW::InputMode::Cursor, LibGLFW::CursorMode::Disabled) }
+    end
+
+    # Checks whether the cursor is invisible when it is over the content area of the window.
+    #
+    # See also: `#hide`
+    def hidden?
+      mode.hidden?
+    end
+
+    # Makes the cursor invisible when it is over the content area of the window
+    # but does not restrict the cursor from leaving.
+    #
+    # Possible errors that could be raised are: `NotInitializedError` and `PlatformError`.
+    #
+    # See also: `#hidden?`
+    def hide
+      checked { LibGLFW.set_input_mode(@pointer, LibGLFW::InputMode::Cursor, LibGLFW::CursorMode::Hidden) }
+    end
+
+    # Checks whether the cursor is visible and behaving normally.
+    #
+    # See also: `#show`
+    def visible?
+      mode.normal?
+    end
+
+    # Makes the cursor visible and behave normally.
+    #
+    # Possible errors that could be raised are: `NotInitializedError` and `PlatformError`.
+    #
+    # See also: `#visible?`
+    def show
+      checked { LibGLFW.set_input_mode(@pointer, LibGLFW::InputMode::Cursor, LibGLFW::CursorMode::Normal) }
+    end
+
+    # Indicates whether stick mouse buttons is enabled.
+    # If sticky mouse buttons are enabled,
+    # a mouse button press will ensure that `#button` (and variants)
+    # returns `ButtonState::Pressed` the next time it is called
+    # even if the mouse button had been released before the call.
+    # This is useful when you are only interested in whether mouse buttons have been pressed
+    # but not when or in which order.
+    #
+    # See also: `#sticky=`
+    def sticky?
+      value = expect_truthy { LibGLFW.get_input_mode(@pointer, LibGLFW::InputMode::StickyMouseButtons) }
+      int_to_bool(value)
+    end
+
+    # Enables or disables sticky mouse buttons.
+    # If sticky mouse buttons are enabled,
+    # a mouse button press will ensure that `#button` (and variants)
+    # returns `ButtonState::Pressed` the next time it is called
+    # even if the mouse button had been released before the call.
+    # This is useful when you are only interested in whether mouse buttons have been pressed
+    # but not when or in which order.
+    #
+    # Whenever you poll state (via `#button`),
+    # you risk missing the state change you are looking for.
+    # If a pressed mouse button is released again before you poll its state,
+    # you will have missed the button press.
+    # The recommended solution for this is to use `#on_button`,
+    # but there is also the sticky mouse buttons input mode.
+    #
+    # Possible errors that could be raised are: `NotInitializedError` and `PlatformError`.
+    #
+    # See also: `#sticky?`
+    def sticky=(flag)
+      value = bool_to_int(flag)
+      checked { LibGLFW.set_input_mode(@pointer, LibGLFW::InputMode::StickyMouseButtons, value) }
+    end
+
     # Checks whether raw mouse motion is supported on the current system.
     # This status does not change after GLFW has been initialized
     # so you only need to check this once.
