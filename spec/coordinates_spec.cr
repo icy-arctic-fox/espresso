b182ebe20e86b12d07ba317aacac237e61ca74ac
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-24 22:03:23 -0600

    Add coordinates type (similar to position)

---
+++ spec/coordinates_spec.cr
@@ -0,0 +1,31 @@
+require "./spec_helper"
+
+Spectator.describe Espresso::Coordinates do
+  let(x) { 42.42 }
+  let(y) { 24.24 }
+  subject(position) { described_class.new(x, y) }
+
+  it "stores the x-coordinate" do
+    expect(position.x).to eq(x)
+  end
+
+  it "stores the y-coordinate" do
+    expect(position.y).to eq(y)
+  end
+
+  describe "#to_s" do
+    subject { position.to_s }
+
+    it "is formatted correctly" do
+      is_expected.to match(/^\(\d+\.\d+, \d+\.\d+\)$/)
+    end
+
+    it "contains the x-coordinate" do
+      is_expected.to contain("#{x}, ")
+    end
+
+    it "contains the y-coordinate" do
+      is_expected.to contain(", #{y}")
+    end
+  end
+end
---
+++ src/espresso/coordinates.cr
@@ -0,0 +1,25 @@
+module Espresso
+  # Two-dimensional position of an object.
+  # Contains an x and y-coordinate
+  # represented as 64-bit floating-pointer numbers.
+  struct Coordinates
+    # Distance along the x-axis.
+    getter x : Float64
+
+    # Distance along the y-axis.
+    getter y : Float64
+
+    # Creates the position with initial values.
+    def initialize(@x, @y)
+    end
+
+    # Creates a string representation of the position.
+    def to_s(io)
+      io << '('
+      io << @x
+      io << ", "
+      io << @y
+      io << ')'
+    end
+  end
+end
