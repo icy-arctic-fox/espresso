070d80e3101773c73ad7b2bf1bf8703e450a9660
Michael Miller <icy.arctic.fox@gmail.com>
2022-01-30 10:17:39 -0700

    Install latest GLFW - 3.3.6

--- install-glfw.sh
+++ install-glfw.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Configuration.
-readonly glfw_version=3.3
+readonly glfw_version=3.3.6
 readonly glfw_git_url=https://github.com/glfw/glfw.git
 readonly glfw_src_dir=glfw
 readonly cmake_args="-DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF"
