c0269e2d6c1b26112fa8f82419e1ff3a4b38ac67
Michael Miller <icy.arctic.fox@gmail.com>
2022-03-01 20:27:14 -0700

    Store paths as immutable slice

--- src/espresso/events/window_drop_event.cr
+++ src/espresso/events/window_drop_event.cr
@@ -4,14 +4,13 @@ module Espresso
   # Event triggered when one or more files are dropped onto the window.
   struct WindowDropEvent < WindowEvent
     # List of file paths.
-    getter paths : Array(String)
+    getter paths : Slice(String)
 
     # Creates the event.
-    protected def initialize(pointer, count, paths)
+    protected def initialize(pointer, count, paths_pointer)
       super(pointer)
-      # Use Slice for safer and easier pointer access.
-      slice = Slice.new(paths, count, read_only: true)
-      @paths = Array.new(count) { |i| String.new(slice[i]) }
+      slice = Slice.new(paths_pointer, count, read_only: true)
+      @paths = slice.map(read_only: true) { |str_ptr| String.new(str_ptr) }
     end
   end
 end
